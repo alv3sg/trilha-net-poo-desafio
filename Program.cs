bef2fdc806d67eaec853a2ee2839fc5eba542885
Gabriel <gabriel.alves.rosario@gmail.com>
2023-12-06 10:05:48 +0000

    conclusão-projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("+351927383060", "A34", "123A5", 16);
+Iphone iphone = new Iphone("+351927383052", "XS", "123AS", 16);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
